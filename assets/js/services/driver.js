e7b58f073727e45df5a8189cb11156960310ca1d
Realm <realm.x@hotmail.com>
2016-11-10 20:20:01 +0800

    clean files

--- assets/js/services/driver.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').factory('Driver', ["$resource", "Config", function ($resource, Config) {
-    return $resource(Config.API + '/driver', {}, {
-        enum: {
-            method: 'POST',
-            url: Config.API + '/driver/enum'
-        }
-    });
-}]);
